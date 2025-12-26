846e720cb16c57d48c27fc568c83d84220673631
pEngwin <137907908+1pEngwin1@users.noreply.github.com>
2025-12-26 12:11:14 +0300

    laba2.1

---
+++ laba2.1/Lab_sem3_02/AssemblyInfo.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
